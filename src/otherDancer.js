4e1e2e5342bbb6bdbe8906696ef0ba1bf860a565
rmaierlit <bbblittlejohn@gmail.com>
2016-11-07 16:04:15 -0800

    Added functionality to dancer and blinky dancer

---
+++ src/otherDancer.js
@@ -0,0 +1,14 @@
+var otherDancer = function(top, left, timeBetweenSteps) {
+  Dancer.call(this, top, left, timeBetweenSteps);
+};
+
+// BlinkyDancer.prototype = Object.create(Dancer.prototype);
+// BlinkyDancer.prototype.constructor = BlinkyDancer;
+
+// // BlinkyDancer.prototype.oldStep = Dancer.prototype.step;
+// BlinkyDancer.prototype.oldStep = BlinkyDancer.prototype.step;
+// BlinkyDancer.prototype.step = function() {
+
+//   this.oldStep();
+//   this.$node.toggle();
+// };
